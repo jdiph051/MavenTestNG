ec09867685daf6e6e9acf48a9e653c6121b52c7f
A232056 <A232056@CIBL273310.scmbdirectory.com>
2017-11-09 08:21:46 +0200

    #Demo-Selenium-Maven-TestNG

---
+++ com.test.maven.selenium/src/test/java/com/test/maven/selenium/DemoForMaven.java
@@ -0,0 +1,32 @@
+package com.test.maven.selenium;
+
+import org.testng.annotations.Test;
+
+
+public class DemoForMaven {
+	
+	@Test
+	public void MethodOne () {
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Method #1 :-)");
+		System.out.println("");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%");
+
+	}
+	
+	@Test
+	public void MethodTwo() {
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("");
+		System.out.println("Method #2 :-)");
+		System.out.println("");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+
+	}
+	
+
+}
